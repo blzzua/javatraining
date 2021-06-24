7c18bea38e9c231b3bf5eee262038f4409e7d170
blzzua <blzzua@github.com>
2021-06-24 12:25:27 +0300

    static field

---
+++ src/main/java/nav/theory/TestStatic.java
@@ -0,0 +1,5 @@
+package nav.theory;
+
+public class TestStatic {
+    public static int i;
+}
--- src/main/java/nav/theory/TheoryClass.java
+++ src/main/java/nav/theory/TheoryClass.java
@@ -3,7 +3,12 @@ package nav.theory;
 public class TheoryClass {
 
     public static void main(String... args) {
-
-
+        TestStatic ts1 = new TestStatic();
+        ts1.i = 14;
+        System.out.println(ts1.i);
+        TestStatic ts2 = new TestStatic();
+        ts2.i = 41;
+        System.out.println(ts1.i);
     }
+
 }
